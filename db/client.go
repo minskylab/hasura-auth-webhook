2ba61139da1804956fbe1bb17b371bb4fb3f2c04
Bregy Malpartida <bregy@minsky.cc>
2021-10-03 19:28:44 -0500

    feat: added base for multimodal database mode

--- db/client.go
+++ db/client.go
@@ -2,11 +2,30 @@ package db
 
 import (
 	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/minskylab/hasura-auth-webhook/config"
 	"github.com/minskylab/hasura-auth-webhook/ent"
 	"github.com/pkg/errors"
 )
+
+func urlParser(connectionString string) (string, error) {
+	uri, err := url.Parse(connectionString)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	host := uri.Hostname()
+	port := uri.Port()
+	user := uri.User.Username()
+	password, _ := uri.User.Password()
+	dbName := strings.Trim(uri.Path, "/")
+
+	config := "host=%s port=%s user=%s dbname=%s password=%s sslmode=disable"
+
+	return fmt.Sprintf(config, host, port, user, dbName, password), nil
+}
 
 func connectionString(c *config.Config) (string, error) {
 	host := c.DBHost
