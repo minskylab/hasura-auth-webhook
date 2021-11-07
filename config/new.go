eb50432a8d4abe48be027eaf5ab978476b27a90b
Bregy Malpartida <bregy@minsky.cc>
2021-11-07 05:45:05 -0500

    feat: change config dependecy and other little upgrades

---
+++ config/new.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
+	"github.com/pkg/errors"
+)
+
+func NewConfig(filepath string) (*Config, error) {
+	config.WithOptions(config.ParseEnv)
+	config.AddDriver(yaml.Driver)
+	config.WithOptions(func(opt *config.Options) {
+		opt.TagName = "yaml"
+	})
+
+	if err := config.LoadFiles(filepath); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	c := new(Config)
+
+	return c, config.BindStruct("", c)
+}
