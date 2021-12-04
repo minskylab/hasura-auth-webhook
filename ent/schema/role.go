060d118aec62e05a5453910f39e8695c78325a5b
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-12-03 23:39:09 -0500

    fix: default false por roles public column

--- ent/schema/role.go
+++ ent/schema/role.go
@@ -22,7 +22,7 @@ func (Role) Fields() []ent.Field {
 		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
 
 		field.String("name").Unique(),
-		field.Bool("public").Optional(),
+		field.Bool("public").Default(false),
 	}
 }
 
